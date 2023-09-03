fd54b1bbacd01904a9e6f7773b62b8d51d30d4ce
Filnis <filippo.nisoli@gmail.com>
2023-09-03 22:45:25 +0200

    solves exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+/* const repeatString = function(string, num) {
+    let result=""
+    for(let i=0; i<num; i++){
+        result+=string
+    }
+    return result
 };
+ */
+const repeatString = function(string, num) {
+    
+    if(num < 0){return 'ERROR'}
+
+    if(num === 0 ){
+        return ''
+    } else {
+        return string+repeatString(string, num-1)
+    }
+};
+
 
 // Do not edit below this line
 module.exports = repeatString;
