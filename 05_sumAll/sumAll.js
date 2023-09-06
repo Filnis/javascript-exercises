234eb9d4d21d00e213fa3634df5d6fd604d1ff72
Filnis <filippo.nisoli@gmail.com>
2023-09-06 20:34:04 +0200

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let uBound = arg1 > arg2 ? arg1 : arg2;
+    let lBound = arg1 > arg2 ? arg2 : arg1;
+    if(lBound < 0 || !Number.isInteger(arg1) || !Number.isInteger(arg2) ){
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    for(let i = 0; i <= uBound - lBound; i++){
+        sum += lBound + i;
+    }
+    return sum
 };
 
 // Do not edit below this line
