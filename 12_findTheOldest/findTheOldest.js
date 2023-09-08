d3c9890039414e479ccf33cbfeff82db113d77e1
Filnis <filippo.nisoli@gmail.com>
2023-09-09 00:39:11 +0200

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
-};
-
+function getAge(person) {
+    if(typeof person.yearOfDeath === "undefined"){
+      return new Date().getFullYear() - person.yearOfBirth;
+    } else {
+      return person.yearOfDeath - person.yearOfBirth;
+    }
+  }
+  
+  const findTheOldest = function(people) {
+    let oldest = people[0];
+    people.forEach(person => {
+      if(getAge(person) > getAge(oldest)){
+        oldest = person;
+      }
+    })
+    return oldest;
+  };
 // Do not edit below this line
 module.exports = findTheOldest;
