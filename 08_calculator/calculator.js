cb9c340d1bfef6f072a0863e2f7b303c5000e625
Filnis <filippo.nisoli@gmail.com>
2023-09-08 21:34:14 +0200

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  arr.forEach(element => {
+    prod *= element;
+  });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0 || num === 1){
+    return 1;
+  } else{
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
